3bbafe5240b8153c17796686d7c606fb7907c070
nguyen-gia-huy <nguyenhuy3112005@gmail.com>
2025-12-07 02:31:35 +0700

    add query client provider

---
+++ userInterface/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import AppRoutes from './router/app-routers';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        {" "}
+        <AppRoutes />
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
+}
+
+export default App
---
+++ userInterface/src/router/app-routers.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AppRoutes = () => {
+  return (
+    <div>AppRouters</div>
+  )
+}
+
+export default AppRoutes
