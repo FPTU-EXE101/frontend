08e68e2774c14b70b49e9ed46879f365241e5641
nguyen-gia-huy <nguyenhuy3112005@gmail.com>
2026-03-12 17:11:39 +0700

    remove staff

--- userInterface/src/router/app-routers.tsx
+++ userInterface/src/router/app-routers.tsx
@@ -2,7 +2,7 @@ import Mainlayout from "@/layout";
 import AdminLayout from "@/pages/Admin/adminLayout";
 import Home from "@/pages/home";
 import ManagerLayout from "@/pages/Manager/managerLayout";
-import StaffLayout from "@/pages/Staff/staffLayout";
+
 import NotFound from "@/pages/NotFound";
 // import UserInfoLayout from "@/pages/User/userInfoLayout";
 import { Route, Routes } from "react-router-dom";
@@ -24,7 +24,7 @@ const AppRoutes = () => {
         {/* <Route path="/user" element={<UserInfoLayout />}></Route> */}
       </Route>
       <Route path="admin" element={<AdminLayout />}></Route>
-      <Route path="staff" element={<StaffLayout />}></Route>
+   
       <Route path="manager" element={<ManagerLayout />}></Route>
       {/* 404 Page - must be last */}
       <Route path="*" element={<NotFound />} />
